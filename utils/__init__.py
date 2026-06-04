2f973b36475980738d3633a75ecb8bdb9d7c5a82
rainx23 <114635269+rainx23@users.noreply.github.com>
2026-06-05 02:29:05 +0800

    docs: add beginner comments

--- utils/__init__.py
+++ utils/__init__.py
@@ -10,6 +10,7 @@ from config.setting import ensure_path_sep
 from utils.other_tools.models import Config
 
 
+# 项目启动或测试导入 utils 时，会先读取 config/config.yaml。
+# 读取后的字典会转换成 Config 对象，后续可以用 config.host 这种方式访问配置。
 _data = GetYamlData(ensure_path_sep("\\config\\config.yaml")).get_yaml_data()
 config = Config(**_data)
-
